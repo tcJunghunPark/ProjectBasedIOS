28456bb030a79c972fbf5347005528428e62f47d
Junghun Park <tacohun21@gmail.com>
2022-03-07 11:45:16 +0900

    일기장 생성 기능

---
+++ Basics/Diary/Diary/Diary.swift
@@ -0,0 +1,14 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by Junghun Park on 2022/03/07.
+//
+
+import Foundation
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
--- Basics/Diary/Diary/DiaryCell.swift
+++ Basics/Diary/Diary/DiaryCell.swift
@@ -9,4 +9,13 @@ import UIKit
 
 class DiaryCell: UICollectionViewCell {
     
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.contentView.layer.cornerRadius = 3.0
+        self.contentView.layer.borderWidth = 1.0
+        self.contentView.layer.borderColor = UIColor.black.cgColor
+    }
 }
