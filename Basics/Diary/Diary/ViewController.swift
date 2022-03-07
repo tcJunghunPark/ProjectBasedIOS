63ba7d261c8cad2b70313df3e5a215bde5f1d07a
Junghun Park <tacohun21@gmail.com>
2022-03-07 12:18:39 +0900

    일기 수정 기능

--- Basics/Diary/Diary/ViewController.swift
+++ Basics/Diary/Diary/ViewController.swift
@@ -21,7 +21,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.configureCollectionView()
         self.loadDiaryList()
+        NotificationCenter.default.addObserver(self, selector: #selector(editDiaryNotification(_:)), name: NSNotification.Name("editDiary"), object: nil)
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func editDiaryNotification(_ notification: Notification){
+        guard let diary = notification.object as? Diary else {return}
+        guard let row = notification.userInfo?["indexPath.row"] as? Int else {return}
+        self.diaryList[row] = diary
+        self.diaryList = self.diaryList.sorted(by: {
+            $0.date.compare($1.date) == .orderedDescending
+        })
+        self.collectionView.reloadData()
     }
     
     private func configureCollectionView(){
