303903c523b141575f266ac7bdf77899268f4bd2
Junghun Park <tacohun21@gmail.com>
2022-03-09 18:24:12 +0900

    코로나 현황판 기본 UI

---
+++ Basics/Covid-19/Covid-19/CovidDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CovidDetailViewController.swift
+//  Covid-19
+//
+//  Created by Junghun Park on 2022/03/09.
+//
+
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+
+    @IBOutlet var newCaseCell: UITableViewCell!
+    @IBOutlet var totalCaseCell: UITableViewCell!
+    @IBOutlet var recoveredCell: UITableViewCell!
+    @IBOutlet var deathCell: UITableViewCell!
+    @IBOutlet var percentageCell: UITableViewCell!
+    @IBOutlet var overseasInflowCell: UITableViewCell!
+    @IBOutlet var regionalOutbreakCell: UITableViewCell!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Basics/Covid-19/Covid-19/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Covid-19
+//
+//  Created by Junghun Park on 2022/03/09.
+//
+
+import UIKit
+import Charts
+
+class ViewController: UIViewController {
+
+    @IBOutlet var totalCaseLabel: UILabel!
+    @IBOutlet var newCaseLabel: UILabel!
+    
+    @IBOutlet var pieChartView: PieChartView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
