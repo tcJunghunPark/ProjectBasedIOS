a3a011b3164788f189fa9acf5f9aad162dadea2c
Junghun Park <tacohun21@gmail.com>
2022-03-09 16:36:11 +0900

    날씨 앱 url session data fetch

--- Basics/Weather/Weather/ViewController.swift
+++ Basics/Weather/Weather/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cityNameTextField: UITextField!
+    @IBOutlet var cityNameLabel: UILabel!
+    @IBOutlet var weatherDescriptionLabel: UILabel!
+    @IBOutlet var tempLabel: UILabel!
+    
+    @IBOutlet var maxTempLabel: UILabel!
+    @IBOutlet var minTempLabel: UILabel!
+    
+    @IBOutlet var weatherStackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapFetchWeatherButton(_ sender: UIButton) {
+        if let cityName = self.cityNameTextField.text {
+            self.getCurrentWeather(cityName: cityName)
+            self.view.endEditing(true)
+        }
+    }
+    func getCurrentWeather(cityName: String){
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=7c07d1206a1a05f8f989c10ed50e006f") else {return}
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url) { [weak self] data, esponse, error in
+            guard let data = data, error == nil else {return}
+            let decoder = JSONDecoder()
+            guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else {return}
+            DispatchQueue.main.async {
+                self?.weatherStackView.isHidden = false
+                self?.configureView(weatherInformation: weatherInformation)
+            }
+        }.resume()
+    }
+    
+    func configureView(weatherInformation: WeatherInformation){
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather = weatherInformation.weather.first {
+            self.weatherDescriptionLabel.text = weather.description
+        }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15))°C"
+        self.minTempLabel.text = "최저: \(Int(weatherInformation.temp.minTemp - 273.15))°C"
+        self.maxTempLabel.text = "최고: \(Int(weatherInformation.temp.maxTemp - 273.15))°C"
+    }
+    
 }
 
---
+++ Basics/Weather/Weather/WeatherInformation.swift
@@ -0,0 +1,41 @@
+//
+//  WeatherInformation.swift
+//  Weather
+//
+//  Created by Junghun Park on 2022/03/09.
+//
+
+import Foundation
+struct WeatherInformation: Codable {
+    let weather: [Weather]
+    let temp: Temp
+    let name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case weather
+        case temp = "main"
+        case name
+    }
+}
+
+struct  Weather: Codable{
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct Temp: Codable {
+    let temp: Double
+    let feelsLike: Double
+    let minTemp: Double
+    let maxTemp: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case minTemp = "temp_min"
+        case maxTemp = "temp_max"
+    }
+}
+
